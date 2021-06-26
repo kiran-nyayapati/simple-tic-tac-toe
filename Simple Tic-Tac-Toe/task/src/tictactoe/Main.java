afd2142a628269d6f6d6464b548af6676cc8a856
Kiran Nyayapati <kiran.nyayapati@gmail.com>
2021-06-26 16:37:43 +0530

    First commit

---
+++ Simple Tic-Tac-Toe/task/src/tictactoe/Main.java
@@ -0,0 +1,9 @@
+package tictactoe;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("X O X\n");
+        System.out.print("O X O\n");
+        System.out.print("X X O\n");
+    }
+}
---
+++ Topics/Calling a method/Area of parallelogram/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        countAreaOfParallelogram(10, 8);
+    }
+
+    // Do not change code below   
+    public static void countAreaOfParallelogram(int b, int h) {
+        System.out.println(b * h);
+    }
+}
