e22c1e74c4a1bcf7d70e026c95a8ad80cf06709a
Kiran Nyayapati <kiran.nyayapati@gmail.com>
2021-06-27 04:05:25 +0530

    3X3 game grid based on input

--- Simple Tic-Tac-Toe/task/src/tictactoe/Main.java
+++ Simple Tic-Tac-Toe/task/src/tictactoe/Main.java
@@ -1,9 +1,26 @@
 package tictactoe;
 
+import java.util.Scanner;
+
 public class Main {
+
+    static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        System.out.print("X O X\n");
-        System.out.print("O X O\n");
-        System.out.print("X X O\n");
+        System.out.print("Enter cells: ");
+        String inputGrid = scanner.nextLine();
+
+        System.out.println("---------");
+        int index = 0;
+
+        for (int i = 1; i <= 3; i++) {
+            System.out.print("| ");
+            for(int j = 1; j <= 3; j++) {
+                char impression = inputGrid.charAt(index++);
+                System.out.print(impression + " ");
+            }
+            System.out.print("|\n");
+        }
+        System.out.println("---------");
     }
 }
