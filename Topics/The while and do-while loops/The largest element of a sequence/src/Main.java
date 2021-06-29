54a8252f6d2ccca885df2e24133bcddbbfee5921
Kiran Nyayapati <kiran.nyayapati@gmail.com>
2021-06-29 23:16:32 +0530

    added program to find the largest element of a sequence

--- Topics/The while and do-while loops/The largest element of a sequence/src/Main.java
+++ Topics/The while and do-while loops/The largest element of a sequence/src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 class Main {
     public static void main(String[] args) {
-        // put your code here
+        Scanner scanner = new Scanner(System.in);
+        
+        int maxNumber = 0;
+        int userInput;
+        
+        do {
+            userInput = scanner.nextInt();          
+            if (userInput > maxNumber) {
+                maxNumber = userInput;
+            }    
+        } while (userInput != 0);
+        
+        System.out.format("%d", maxNumber);
     }
 }
