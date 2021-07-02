4432651e73a88198410e90c7c9d16c5aa68dafc7
Kiran Nyayapati <kiran.nyayapati@gmail.com>
2021-07-03 00:25:44 +0530

    added sequence program

--- Topics/Branching statements/The sequence/src/Main.java
+++ Topics/Branching statements/The sequence/src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 class Main {
     public static void main(String[] args) {
-        // put your code here
+        Scanner scanner = new Scanner(System.in);
+
+        int number = scanner.nextInt(); 
+        int count = 1;
+
+        for (int i = 0; i <= number; i++) {
+            for (int j = 0; j < i; j++) {
+                if (count > number) {
+                    break;
+                }
+                System.out.print(i + " ");
+                count++;
+            }
+        }
     }
 }
