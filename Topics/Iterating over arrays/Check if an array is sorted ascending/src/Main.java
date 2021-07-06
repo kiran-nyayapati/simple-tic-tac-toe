f19b446e9c3144f293b9b7c31ca98a8994078fdc
Kiran Nyayapati <kiran.nyayapati@gmail.com>
2021-07-06 23:47:34 +0530

    added program to find min value of array and to check if an array is sorted ascending

--- Topics/Iterating over arrays/Check if an array is sorted ascending/src/Main.java
+++ Topics/Iterating over arrays/Check if an array is sorted ascending/src/Main.java
@@ -1,23 +1,24 @@
 import java.util.Scanner;
 
 class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) { 
         Scanner scanner = new Scanner(System.in);
-
+        
         int size = scanner.nextInt();
         int[] integerNumbersArray = new int[size];
 
         for (int i = 0; i < size; i++) {
             integerNumbersArray[i] = scanner.nextInt();
         }
-
-        int minValue = integerNumbersArray[0];
-
+        
+        boolean isArraySorted = true;
+             
         for (int i = 0; i < integerNumbersArray.length - 1; i++) {
-            if (integerNumbersArray[i + 1] < minValue) {
-                minValue = integerNumbersArray[i + 1];
+            if (integerNumbersArray[i] > integerNumbersArray[i + 1]) {
+                isArraySorted = false;
+                break;
             }
-        }
-        System.out.format("%d", minValue);
+        }     
+        System.out.println(isArraySorted);
     }
 }
