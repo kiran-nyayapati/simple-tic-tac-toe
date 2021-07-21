a989477ee64f255267d2faa47368228223fe3391
Kiran Nyayapati <kiran.nyayapati@gmail.com>
2021-07-21 20:32:03 +0530

    stage 3 - whats up on the field

--- Simple Tic-Tac-Toe/task/src/tictactoe/Main.java
+++ Simple Tic-Tac-Toe/task/src/tictactoe/Main.java
@@ -1,26 +1,88 @@
 package tictactoe;
 
 import java.util.Scanner;
-
 public class Main {
-
+    static char[] grid;
     static Scanner scanner = new Scanner(System.in);
-
     public static void main(String[] args) {
         System.out.print("Enter grid: ");
-        String inputGrid = scanner.nextLine();
+        String inputGrid = scanner.next();
+        grid = inputGrid.toCharArray();
+        showGame();
+        analyzeGame();
+    }
 
+    public static void showGame() {
         System.out.println("---------");
-        int index = 0;
+        System.out.format("| %c %c %c |\n", grid[0], grid[1], grid[2]);
+        System.out.format("| %c %c %c |\n", grid[3], grid[4], grid[5]);
+        System.out.format("| %c %c %c |\n", grid[6], grid[7], grid[8]);
+        System.out.println("---------");
+    }
 
-        for (int i = 1; i <= 3; i++) {
-            System.out.print("| ");
-            for(int j = 1; j <= 3; j++) {
-                char impression = inputGrid.charAt(index++);
-                System.out.print(impression + " ");
-            }
-            System.out.print("|\n");
+    public static void analyzeGame() {
+        boolean impossible;
+        boolean threeX;
+        boolean threeO;
+        boolean aGridEmpty;
+
+        impossible = invalidGrid();
+        threeO = isThree('O');
+        threeX = isThree('X');
+        aGridEmpty = isAGridEmpty();
+
+        if (impossible || (threeO && threeX)) {
+            System.out.println("Impossible");
         }
-        System.out.println("---------");
+        else if (threeO) {
+            System.out.println("O wins");
+        }
+        else if (threeX) {
+            System.out.println("X wins");
+        }
+        else if (aGridEmpty) {
+            System.out.println("Game not finished");
+        }
+        else {
+            System.out.println("Draw");
+        }
+    }
+
+    public static boolean isThree(char element) {
+        if (isThreeInARow(grid[0], grid[4], grid[8], element) ||
+                isThreeInARow(grid[6], grid[4], grid[2], element)) {
+            return true;
+        }
+
+        for (int i = 0; i < 3; i++) {
+            if (isThreeInARow(grid[i], grid[i + 3], grid[i + 6], element)) return true;
+            if (isThreeInARow(grid[i * 3], grid[i * 3 + 1], grid[i * 3 + 2], element)) return true;
+        }
+        return false;
+    }
+
+    public static boolean isThreeInARow(char a, char b, char c, char element) {
+        return a == b && b == c && b == element;
+    }
+
+    public static boolean invalidGrid(){
+        int countO = 0;
+        int countX = 0;
+        for (char ch: grid) {
+            if (ch == 'O') {
+                countO++;
+            }
+            else if (ch == 'X'){
+                countX++;
+            }
+        }
+        return Math.abs(countO - countX) > 1;
+    }
+
+    public static boolean isAGridEmpty() {
+        for (char ch: grid) {
+            if (ch == '_') return true;
+        }
+        return false;
     }
 }
