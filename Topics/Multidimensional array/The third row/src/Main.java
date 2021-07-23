cca3e99170e761692e698f7e0c29d5fd9c5c62d5
Kiran Nyayapati <kiran.nyayapati@gmail.com>
2021-07-23 20:39:29 +0530

    third row - multidimensional array

---
+++ Topics/Multidimensional array/The third row/src/Main.java
@@ -0,0 +1,7 @@
+class ArrayOperations {
+    public static void printTheThirdRow(int[][] twoDimArray) {
+        for (int j = 0; j < twoDimArray[2].length; j++) {
+            System.out.print(twoDimArray[2][j] + " ");
+        }
+    }
+}
